a52f7ceda1d9d521d551cc75348cb38763788409
k-souvatzidaki <kwn.souv@gmail.com>
2021-05-24 15:54:00 +0300

    Add static content path

--- 2/index.js
+++ 2/index.js
@@ -1,1 +1,17 @@
-console.log("Hello world")
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.listen(8080)
+
+//static content in directory "public", accessible under path "/static"
+app.use('/static', express.static(__dirname + '/public'))
+//index.html is the root page of this app
+app.get('/', function(req, res){
+    var options = {
+        root: path.join(__dirname, 'public')
+    }
+    res.sendFile('index.html', options, function(err){
+        console.log(err)
+    })
+})
