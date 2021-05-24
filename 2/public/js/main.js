61b1d65f5234d6f842f744a5d45db6f809e2549b
k-souvatzidaki <kwn.souv@gmail.com>
2021-05-24 12:39:00 +0300

    Add favorites buttons

--- 2/public/js/main.js
+++ 2/public/js/main.js
@@ -1,18 +1,22 @@
-//handlebars template
+//The Handlebars template to view works
 var templates = {}
 templates.workDetails = Handlebars.compile(`
 {{#each work}}
     <section>
         <p>{{authorweb}} {{titleweb}}</p>
+        <input type="button" class="favorite" value="+ Αγαπημένα">
     </section>
 {{/each}}
 `)
 
+//Add event listener when the website is loaded
 window.addEventListener('load',function() {
     let submit_button = document.querySelector("[type=button]")
     submit_button.addEventListener('click',fetch_url)
 })
 
+//fetch a list of all works given a search term
+//and view them to the website with a Handlebars template
 function fetch_url() {
     let searchbox = document.querySelector("[type=text]")
     let input = searchbox.value
@@ -33,14 +37,24 @@ function fetch_url() {
         }
     })
     .then(obj => {
-        //view json objects content in webpage here with handlebars
-        console.log('Received',obj)
-        let main = document.querySelector("article")
+        console.log('Received',obj) //TODO remove
+
+        //view json objects content in webpage with Handlebars
+        let results = document.querySelector("article")
         let content =  templates.workDetails(obj)
-        main.innerHTML = content
-        //document.body.innerHTML = obj.work[0].authorweb; //gets the name of the author for first work returned
+        results.innerHTML = content
+        //add event listener to new buttons
+        let add_to_faves = document.querySelectorAll(".favorite")
+        for(var i=0; i < add_to_faves.length;i++ ) {
+            add_to_faves[i].addEventListener('click',favorite)
+        }
     })
     .catch(error => {
         console.log(error)
     })
+}
+
+//click listener for add to favorites button
+function favorite() {
+    console.log("Click!")
 }
